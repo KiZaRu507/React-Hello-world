98e99385742f9a6d0abe1ee32372c6f451211443
Sumukh VR <vrsumukh32@gmail.com>
2021-12-17 01:11:09 +0530

    Intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./main.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="App">
+
+      <img className="imag" src="https://images.unsplash.com/photo-1494783367193-149034c05e8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
+      <h3>This is a image</h3>
+      <button onClick={clicked}>Submit</button>
+      
     </div>
+  
   );
 }
+const clicked=()=>{
+  console.log('clicked')
+  alert("clicked");
+}
+
 
 export default App;
